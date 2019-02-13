59a4133541203870a04b7d25670801a20910e09f
John M. Schanck <jschanck@uwaterloo.ca>
2019-02-13 15:56:39 -0500

    Placeholder sort. Borrowed from supercop 'crypto_sort/portable3'.

--- ref-common/hps_sample.c
+++ ref-common/hps_sample.c
@@ -1,4 +1,5 @@
 #include "sample.h"
+#include "crypto_sort.h"
 #include "fips202.h"
 
 void sample_xof(unsigned char *output, const size_t sizeof_output, const unsigned char seed[NTRU_SEEDBYTES], const unsigned char domain[NTRU_DOMAINBYTES])
@@ -24,6 +25,26 @@ void sample_iid(poly *r, const unsigned char uniformbytes[NTRU_S3_IID_BYTES])
   r->coeffs[NTRU_N-1] = 0;
 }
 
-void sample_fixed_type(poly *r, const unsigned char uniformbytes[NTRU_S3_FT_BYTES])
+void sample_fixed_type(poly *r, const unsigned char u[NTRU_S3_FT_BYTES])
 {
+  // XXX: Assumes NTRU_S3_FT_BYTES = 4*N - 4.
+
+  int32_t s[NTRU_N-1];
+  int i;
+
+  for (i = 0; i < NTRU_N-1; i++)
+    s[i] = u[4*i+0] + (u[4*i+1] << 8) + (u[4*i+2] << 16) + (u[4*i+3] << 24);
+
+  for (i = 0; i<NTRU_N-1; i++) s[i] &= -4;
+
+  for (i = 0; i<NTRU_WEIGHT/2; i++) s[i] |=  1;
+
+  for (i = NTRU_WEIGHT/2; i<NTRU_WEIGHT; i++) s[i] |=  2;
+
+  crypto_sort(s,NTRU_N-1);
+
+  for(i=0; i<NTRU_N-1; i++)
+    r->coeffs[i] = ((uint16_t) (s[i] & 3));
+
+  r->coeffs[NTRU_N-1] = 0;
 }
