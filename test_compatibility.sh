7032e892685a10df11d61518fad4af0ccf9b5117
John M. Schanck <jschanck@uwaterloo.ca>
2019-02-13 17:01:18 -0500

    update test_compatibility.sh

--- test_compatibility.sh
+++ test_compatibility.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-make -C ref-sxy {test/keypair,test/encap,test/decap}
-make -C avx2-sxy {test/keypair,test/encap,test/decap}
+make -C ref-hrss701 {test/keypair,test/encap,test/decap}
+make -C avx2-hrss701 {test/keypair,test/encap,test/decap}
 
 PKBYTES=1138
 SKBYTES=1450
@@ -13,9 +13,9 @@ ciphertextandkey=$(mktemp)
 ciphertext=$(mktemp)
 key=$(mktemp)
 
-for keygen in {"ref-sxy","avx2-sxy"}; do
-    for encap in {"ref-sxy","avx2-sxy"}; do
-        for decap in {"ref-sxy","avx2-sxy"}; do
+for keygen in {"ref-hrss701","avx2-hrss701"}; do
+    for encap in {"ref-hrss701","avx2-hrss701"}; do
+        for decap in {"ref-hrss701","avx2-hrss701"}; do
             echo -n "Testing with keygen: ${keygen}, encap: ${encap}, decap: ${decap}.. "
             ${keygen}/test/keypair > ${keyfile}
             head -c ${PKBYTES} ${keyfile} | ${encap}/test/encap > ${ciphertextandkey}
