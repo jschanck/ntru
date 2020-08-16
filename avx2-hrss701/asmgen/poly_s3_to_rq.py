9cad7c4b23ef19f5ce9a0119f6af138f0b1f1f03
John M. Schanck <jschanck@uwaterloo.ca>
2020-08-15 21:52:55 -0400

    avx2: missing namespacing for ntruhrss701 poly_s2_to_rq

--- avx2-hrss701/asmgen/poly_s3_to_rq.py
+++ avx2-hrss701/asmgen/poly_s3_to_rq.py
@@ -81,11 +81,11 @@ if __name__ == '__main__':
         p(".word 0")
 
     p(".text")
-    p(".hidden poly_lift")
-    p(".global poly_lift")
+    p(".hidden {}poly_lift".format(NAMESPACE))
+    p(".global {}poly_lift".format(NAMESPACE))
     p(".att_syntax prefix")
 
-    p("poly_lift:")
+    p("{}poly_lift:".format(NAMESPACE))
 
     p("mov %rsp, %r8")  # Use r8 to store the old stack pointer during execution.
     p("andq $-32, %rsp")  # Align rsp to the next 32-byte value, for vmovdqa.
