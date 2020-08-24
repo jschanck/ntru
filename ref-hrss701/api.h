5d805377168a2ffb9cd1e6ef7f0e582aa9662fdd
John M. Schanck <jschanck@uwaterloo.ca>
2020-08-24 09:45:05 -0400

    Namespacing in api.h

--- ref-hrss701/api.h
+++ ref-hrss701/api.h
@@ -8,10 +8,13 @@
 
 #define CRYPTO_ALGNAME "ntruhrss701"
 
+#define crypto_kem_keypair CRYPTO_NAMESPACE(keypair)
 int crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
 
+#define crypto_kem_enc CRYPTO_NAMESPACE(enc)
 int crypto_kem_enc(unsigned char *c, unsigned char *k, const unsigned char *pk);
 
+#define crypto_kem_dec CRYPTO_NAMESPACE(dec)
 int crypto_kem_dec(unsigned char *k, const unsigned char *c, const unsigned char *sk);
 
 #endif
