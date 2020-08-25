3346b0153e1906adcb3004bafa2fa08f471eb8da
John M. Schanck <jschanck@uwaterloo.ca>
2020-08-25 07:57:42 -0400

    Move crypto_sort_int32.h include to top of sample.c

--- ref-common/sample.c
+++ ref-common/sample.c
@@ -1,3 +1,6 @@
+#ifdef NTRU_HPS
+#include "crypto_sort_int32.h"
+#endif
 #include "sample.h"
 
 void sample_fg(poly *f, poly *g, const unsigned char uniformbytes[NTRU_SAMPLE_FG_BYTES])
@@ -58,7 +61,6 @@ void sample_iid_plus(poly *r, const unsigned char uniformbytes[NTRU_SAMPLE_IID_B
 #endif
 
 #ifdef NTRU_HPS
-#include "crypto_sort_int32.h"
 void sample_fixed_type(poly *r, const unsigned char u[NTRU_SAMPLE_FT_BYTES])
 {
   // Assumes NTRU_SAMPLE_FT_BYTES = ceil(30*(n-1)/8)
