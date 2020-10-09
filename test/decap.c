2c98003e4994b99b0d97042862630c662a29f888
John M. Schanck <jschanck@uwaterloo.ca>
2020-10-09 18:04:51 -0400

    test/decap: return result

--- test/decap.c
+++ test/decap.c
@@ -7,11 +7,12 @@ int main() {
     unsigned char* sk = (unsigned char*) malloc(CRYPTO_SECRETKEYBYTES);
     unsigned char* c = (unsigned char*) malloc(CRYPTO_CIPHERTEXTBYTES);
     unsigned char* k = (unsigned char*) malloc(CRYPTO_BYTES);
+    int result;
 
     fread(sk, 1, CRYPTO_SECRETKEYBYTES, stdin);
     fread(c, 1, CRYPTO_CIPHERTEXTBYTES, stdin);
 
-    crypto_kem_dec(k, c, sk);
+    result = crypto_kem_dec(k, c, sk);
 
     fwrite(k, 1, CRYPTO_BYTES, stdout);
 
@@ -21,5 +22,5 @@ int main() {
     free(c);
     free(k);
 
-    return 0;
+    return result;
 }
