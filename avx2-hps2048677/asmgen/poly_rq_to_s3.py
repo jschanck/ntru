b38a346adefe41f680b4d6998d0d5a63f25f0e39
John M. Schanck <jschanck@uwaterloo.ca>
2020-10-12 14:49:43 -0400

    Use simpler poly_Rq_to_S3

--- avx2-hps2048677/asmgen/poly_rq_to_s3.py
+++ avx2-hps2048677/asmgen/poly_rq_to_s3.py
@@ -10,15 +10,6 @@ if __name__ == '__main__':
     p(".data")
     p(".p2align 5")
 
-    p("const_3_repeating:")
-    for i in range(16):
-        p(".word 0x3")
-
-    p("shuf_b8_to_low_doubleword:")
-    for j in range(16):
-        p(".byte 8")
-        p(".byte 255")
-
     p("mask_modq:")
     for i in range(16):
         p(".word {}".format(NTRU_Q-1))
@@ -32,37 +23,51 @@ if __name__ == '__main__':
     p("{}poly_Rq_to_S3:".format(NAMESPACE))
     p("_{}poly_Rq_to_S3:".format(NAMESPACE))
 
-    r = 0
-    a = 1
-    threes = 3
-    last = 4
-    retval = 5
+    a = 0
+    r = 1
+    last = 5
     modq = 6
-    p("vmovdqa const_3_repeating(%rip), %ymm{}".format(threes))
+
     p("vmovdqa mask_modq(%rip), %ymm{}".format(modq))
-    p("vmovdqa {}(%rsi), %ymm{}".format((NTRU_N32 // 16 - 1)*32, last))
+
+    p("vmovdqa {}(%rsi), %ymm{}".format(2*(16*((NTRU_N-1) // 16)), last))
+    # Need explicit reduction mod q in this routine.
     p("vpand %ymm{}, %ymm{}, %ymm{}".format(modq, last, last));
 
+    # Extract the last coefficient
+    quadword_of_last = ((NTRU_N-1)%16)//4
+    if quadword_of_last != 0:
+        p("vpermq ${}, %ymm{}, %ymm{}".format(quadword_of_last, last, last))
+    word_of_low = (NTRU_N-1)%4
+    if word_of_low != 0:
+        p("vpsrlq ${}, %ymm{}, %ymm{}".format(16*word_of_low, last, last))
+    # Copy the last coefficient to high 16 of low double word
+    p("vpslld $16, %ymm{}, %ymm{}".format(last, r))
+    p("vpsrld $16, %ymm{}, %ymm{}".format(r, last))
+    p("vpor %ymm{}, %ymm{}, %ymm{}".format(last, r, last))
+    # Broadcast the last coefficient to all 16 words
+    p("vbroadcastss %xmm{}, %ymm{}".format(last, last))
+
+    # Add (-q) mod 3 to last if last is >= q/2.
+    # Note (-q) mod 3=(-2^k) mod 3=1<<(1-(k&1))
     p("vpsrlw ${}, %ymm{}, %ymm{}".format(LOGQ-1, last, r))
-    p("vpxor %ymm{}, %ymm{}, %ymm{}".format(threes, r, r))
-    p("vpsllw ${}, %ymm{}, %ymm{}".format(LOGQ, r, r))
+    if LOGQ%2==0: p("vpsllw ${}, %ymm{}, %ymm{}".format(1, r, r))
     p("vpaddw %ymm{}, %ymm{}, %ymm{}".format(last, r, last))
 
-    mod3(last, retval)
-    p("vpsllw $1, %ymm{}, %ymm{}".format(retval, last))
-    p("vextracti128 $1, %ymm{}, %xmm{}".format(last, last))
-    p("vpshufb shuf_b8_to_low_doubleword(%rip), %ymm{}, %ymm{}".format(last, last))
-    p("vinserti128 $1, %xmm{}, %ymm{}, %ymm{}".format(last, last, last))
+    # map last to -last mod 3
+    mod3(last, r)
+    p("vpsllw $1, %ymm{}, %ymm{}".format(r, last))
 
     for i in range(NTRU_N32 // 16):
         p("vmovdqa {}(%rsi), %ymm{}".format(i*32, a))
         p("vpand %ymm{}, %ymm{}, %ymm{}".format(modq, a, a));
+
         p("vpsrlw ${}, %ymm{}, %ymm{}".format(LOGQ-1, a, r))
-        p("vpxor %ymm{}, %ymm{}, %ymm{}".format(threes, r, r))
-        p("vpsllw ${}, %ymm{}, %ymm{}".format(LOGQ, r, r))
-        p("vpaddw %ymm{}, %ymm{}, %ymm{}".format(a, r, r))
-        p("vpaddw %ymm{}, %ymm{}, %ymm{}".format(last, r, r))
-        mod3(r, retval)
-        p("vmovdqa %ymm{}, {}(%rdi)".format(retval, i*32))
+        if LOGQ%2==0: p("vpsllw ${}, %ymm{}, %ymm{}".format(1, r, r))
+        p("vpaddw %ymm{}, %ymm{}, %ymm{}".format(a, r, a))
+
+        p("vpaddw %ymm{}, %ymm{}, %ymm{}".format(a, last, a))
+        mod3(a, r)
+        p("vmovdqa %ymm{}, {}(%rdi)".format(r, i*32))
 
     p("ret")
