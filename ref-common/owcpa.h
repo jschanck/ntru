6d64473e78687df606186bc305e2f5f30825a9e9
John M. Schanck <jschanck@uwaterloo.ca>
2020-10-30 22:51:21 -0400

    Clean up owcpa.h
    * Remove unused owcpa_samplemsg declaration
    * Correct owcpa_keypair seed length comment
    
    Thanks to Robert Ransom for pointing out these errors.

--- ref-common/owcpa.h
+++ ref-common/owcpa.h
@@ -4,14 +4,10 @@
 #include "params.h"
 #include "poly.h"
 
-#define owcpa_samplemsg CRYPTO_NAMESPACE(owcpa_samplemsg)
-void owcpa_samplemsg(unsigned char msg[NTRU_OWCPA_MSGBYTES],
-                     const unsigned char seed[NTRU_SEEDBYTES]);
-
 #define owcpa_keypair CRYPTO_NAMESPACE(owcpa_keypair)
 void owcpa_keypair(unsigned char *pk,
                    unsigned char *sk,
-                   const unsigned char seed[NTRU_SEEDBYTES]);
+                   const unsigned char seed[NTRU_SAMPLE_FG_BYTES]);
 
 #define owcpa_enc CRYPTO_NAMESPACE(owcpa_enc)
 void owcpa_enc(unsigned char *c,
